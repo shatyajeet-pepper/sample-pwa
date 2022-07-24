07c53a6f3a79cd471ee6751fa735b24f69cfd8d0
Shatyajeet Parida <shatyajeet.parida@peppercontent.io>
2022-07-24 16:43:54 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx again</code> and save to reload.
+          Edit <code>src/App.tsx again and again</code> and save to reload.
         </p>
         <a
           className="App-link"
